3c8eb8f23d4f9a179eeeb24d3a22ca64dfdad8d6
Joslin Some <joslin-some@uiowa.edu>
2023-03-18 03:20:56 -0500

    bug

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,10 +1,7 @@
 import logo from './logo.svg'
 import React, {useEffect} from 'react';
 import './App.css';
-import Navbar from "./components/NavBar";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Home from "./pages/Home";
-import SellPage from "./pages/SellPage"
+import RoutesNavigation from "./routes/Routes";
 
 
 function App() {
